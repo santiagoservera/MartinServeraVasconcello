fb6497adacd22421d89e46545f9eb7802b25785d
Santiago Servera <santiagoservera21@gmail.com>
2023-09-12 18:45:47 -0300

    States login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,28 +3,39 @@ import './Login.css';
 import React, {useState} from 'react';
 
 export default function Login() {
-    const [usuario, setUsuario]=useState("")
-    console.log("Usuario:",usuario)
-    const [contraseña, setContraseña]=useState("")
-    
-    console.log("Contraseña:",contraseña)
+  const [usuario, setUsuario] = useState("");
+  const [contraseña, setContraseña] = useState("");
+
+  const handleUsuarioChange = (e) => {
+      const nuevoUsuario = e.target.value;
+      setUsuario(nuevoUsuario);
+      console.log("Usuario:", nuevoUsuario);
+  }
+
+  const handleContraseñaChange = (e) => {
+      const nuevaContraseña = e.target.value;
+      setContraseña(nuevaContraseña);
+      console.log("Contraseña:", nuevaContraseña);
+  }
+
   return (
-    <div className="Logincontainer1">
-      <h1>Login</h1>
-      <h2>Usuario</h2>
-      <input className='input'
-            type="text" 
-            onChange={e=> setUsuario(e.target.value)}
+      <div className="Logincontainer1">
+          <h1>Login</h1>
+          <h2>Usuario</h2>
+          <input
+              className='input'
+              type="text"
+              onChange={handleUsuarioChange}
           />
-        <h2>contraseña</h2>
-      <input className='input'
-            type="password" 
-            onChange={e=> setContraseña(e.target.value)}
+          <h2>Contraseña</h2>
+          <input
+              className='input'
+              type="password"
+              onChange={handleContraseñaChange}
           />
-          <Link to={'/Home'} type="submit" class="button" name="btn">
-            Enviar
+          <Link to={'/Home'} type="submit" className="button" name="btn">
+              Enviar
           </Link>
-          
-    </div>
+      </div>
   );
 }
